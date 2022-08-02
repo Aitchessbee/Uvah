e97ff1034bb1d5bf4bcede8cf6d139f50dee67fc
Aitchessbee <bediharsiddak@gmail.com>
2022-08-03 01:17:14 +0530

    added login and register page

--- UVAH/base/urls.py
+++ UVAH/base/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('webdev/', views.webdev, name="webdev"),
+    path('frontend/', views.frontend, name="frontend"),
+    path('backend/', views.backend, name="backend"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
 ]
--- UVAH/base/views.py
+++ UVAH/base/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/index.html')
+    return render(request, 'base/home.html')
+
+def login(request):
+    return render(request, 'base/login.html')
+
+def register(request):
+    return render(request, 'base/register.html')
+
+def webdev(request):
+    return render(request, 'base/webdev.html')
+
+def frontend(request):
+    return render(request, 'base/frontend.html')
+
+def backend(request):
+    return render(request, 'base/backend.html')
+
