1480e046dfd1debca2795e79a91b0465795d06f7
Aitchessbee <bediharsiddak@gmail.com>
2022-08-22 23:19:35 +0530

    made UI improvements

--- UVAH/base/models.py
+++ UVAH/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    domain = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
+    subtopic = models.CharField(max_length=200)
+    course_title = models.CharField(max_length=200)
+    course_author = models.CharField(max_length=200)
+    course_link = models.CharField(max_length=200)
+    course_rating = models.IntegerField(max_length=200)
